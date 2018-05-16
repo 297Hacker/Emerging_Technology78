6daea4d7d5a4ccaa6de71b3494bf790d8796966b
Steven <s.koerts2@gmail.com>
2018-05-16 13:02:04 +0200

    minor change + update gitignore

--- app/tensorflow/init.js
+++ app/tensorflow/init.js
@@ -4,4 +4,3 @@ function test() {
     // test function, to make sure it compiles
 }
 exports.test = test;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5pdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImluaXQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQTtJQUNJLDBDQUEwQztBQUM5QyxDQUFDO0FBRkQsb0JBRUMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZnVuY3Rpb24gdGVzdCgpIHtcclxuICAgIC8vIHRlc3QgZnVuY3Rpb24sIHRvIG1ha2Ugc3VyZSBpdCBjb21waWxlc1xyXG59Il19
